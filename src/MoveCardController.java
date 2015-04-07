260cf6b792f6ac3b88cc176916640976f7396d83
Sonia Martínez Martín <inmakia@gmail.com>
2015-04-08 00:28:32 +0200

    MoveController -> MoveFromDeckToWaste test

--- src/MoveCardController.java
+++ src/MoveCardController.java
@@ -1,18 +1,33 @@
 
 public class MoveCardController {
 
-	public MoveCardController(int i, int j) {
-		// TODO Auto-generated constructor stub
+	private int deckSize, wasteSize;
+	
+	public MoveCardController(int deckSize, int wasteSize) {
+		this.deckSize = deckSize;
+		this.wasteSize = wasteSize;
 	}
 
 	public void moveFromDeckToWaste() {
-		// TODO Auto-generated method stub
-		
+		if(this.deckSize >= 3) {
+			this.deckSize -= 3;
+			this.wasteSize += 3;
+		} else if(this.deckSize == 2) {
+			this.deckSize -= 2;
+			this.wasteSize += 2;
+		} else if(this.deckSize == 1) {
+			this.deckSize -= 1;
+			this.wasteSize += 1;
+		} else if(this.deckSize == 0) {
+		}
 	}
 
 	public int getDeckSize() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.deckSize;
+	}
+
+	public int getWasteSize() {
+		return this.wasteSize;
 	}
 
 }
--- src/MoveCardControllerTest.java
+++ src/MoveCardControllerTest.java
@@ -24,14 +24,26 @@ public class MoveCardControllerTest {
 	
 	@Test
 	public void moveFromDeckToWasteTest() {
-		int oldDeck; 
-		oldDeck = moveCardController1.getDeckSize();
 		moveCardController1.moveFromDeckToWaste();
-		assertEquals(21, oldDeck-3);
-		moveCardController2.moveFromDeckToWaste();
+		assertTrue(moveCardController1.getDeckSize() == 21);
+		assertTrue(moveCardController1.getWasteSize() == 3);
+		
+		moveCardController2.moveFromDeckToWaste();		
+		assertTrue(moveCardController2.getDeckSize() == 0);
+		assertTrue(moveCardController2.getWasteSize() == 3);
+		
 		moveCardController3.moveFromDeckToWaste();
+		assertTrue(moveCardController3.getDeckSize() == 0);
+		assertTrue(moveCardController3.getWasteSize() == 2);
+		
 		moveCardController4.moveFromDeckToWaste();
+		assertTrue(moveCardController4.getDeckSize() == 0);
+		assertTrue(moveCardController4.getWasteSize() == 1);		
+		
 		moveCardController5.moveFromDeckToWaste();
+		assertTrue(moveCardController5.getDeckSize() == 0);
+		assertTrue(moveCardController5.getWasteSize() == 0);
+		
 	}
 
 }
